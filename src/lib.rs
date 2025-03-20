58c93c54c536be19d05c99885836ed55654e2dcf
ianwalker04 <ian04walker@gmail.com>
2025-03-19 21:39:19 -0500

    Finished level 1 implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,17 @@
 
 use num::Integer;
 use pc_keyboard::{DecodedKey, KeyCode};
-use pluggable_interrupt_os::println;
 use pluggable_interrupt_os::vga_buffer::{
     is_drawable, plot, Color, ColorCode, BUFFER_HEIGHT, BUFFER_WIDTH,
 };
 
 use core::{
-    clone::Clone,
-    cmp::{min, Eq, PartialEq},
-    iter::Iterator,
-    marker::Copy,
-    prelude::rust_2024::derive,
+    clone::Clone, cmp::{min, Eq, PartialEq}, iter::Iterator, marker::Copy, prelude::rust_2024::derive
 };
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct SwimInterface {
-    letters: [char; BUFFER_WIDTH],
+    letters: [[char; BUFFER_WIDTH]; BUFFER_HEIGHT],
     num_letters: usize,
     next_letter: usize,
     col: usize,
@@ -40,19 +35,19 @@ pub fn sub1<const LIMIT: usize>(value: usize) -> usize {
 impl Default for SwimInterface {
     fn default() -> Self {
         Self {
-            letters: ['\0'; BUFFER_WIDTH],
+            letters: [['\0'; BUFFER_WIDTH]; BUFFER_HEIGHT],
             num_letters: 0,
             next_letter: 0,
             col: 0,
             row: 0,
-            cursor_position: 1
+            cursor_position: 0
         }
     }
 }
 
 impl SwimInterface {
     fn letter_columns(&self) -> impl Iterator<Item = usize> + '_ {
-        (0..self.num_letters).map(|n| safe_add::<BUFFER_WIDTH>(n, self.col))
+        0..self.num_letters
     }
 
     pub fn tick(&mut self) {
@@ -61,32 +56,28 @@ impl SwimInterface {
     }
 
     fn clear_current(&self) {
-        for _ in self.letter_columns() {
-            plot(' ', self.col, self.row, ColorCode::new(Color::Black, Color::Black));
+        for col in self.letter_columns() {
+            plot(' ', col, self.row, ColorCode::new(Color::Black, Color::Black));
         }
         plot(' ', self.cursor_position, self.row, ColorCode::new(Color::Black, Color::Black));
     }
 
     fn draw_current(&mut self) {
-        if self.cursor_position + 1 == BUFFER_WIDTH {
-            self.start_new_line();
-        }
         for (i, _) in self.letter_columns().enumerate() {
             plot(
-                self.letters[i],
-                self.col,
+                self.letters[self.row][i],
+                i,
                 self.row,
                 ColorCode::new(Color::White, Color::Black),
             );
         }
-        plot('|', self.cursor_position, self.row, ColorCode::new(Color::White, Color::Black));
+        plot(' ', self.cursor_position, self.row, ColorCode::new(Color::White, Color::White));
     }
 
     pub fn start_new_line(&mut self) {
         plot(' ', self.cursor_position, self.row, ColorCode::new(Color::Black, Color::Black));
         self.row = add1::<BUFFER_HEIGHT>(self.row);
-        self.col = 0;
-        self.cursor_position = 1;
+        self.cursor_position = 0;
         self.num_letters = 0;
         self.next_letter = 0;
     }
@@ -100,17 +91,23 @@ impl SwimInterface {
 
     fn handle_raw(&mut self, key: KeyCode) {
         match key {
-            KeyCode::ArrowDown => self.start_new_line(),
             _ => {}
         }
     }
 
     fn handle_unicode(&mut self, key: char) {
-        if is_drawable(key) {
-            self.letters[self.next_letter] = key;
+        if key == '\n' {
+            self.start_new_line();
+        } else if is_drawable(key) {
+            if self.cursor_position >= BUFFER_WIDTH - 1 {
+                let current_char: char = key;
+                self.start_new_line();
+                self.letters[self.row][self.next_letter] = current_char;
+            } else {
+                self.letters[self.row][self.next_letter] = key;
+            }
             self.next_letter = add1::<BUFFER_WIDTH>(self.next_letter);
             self.num_letters = min(self.num_letters + 1, BUFFER_WIDTH);
-            self.col = add1::<BUFFER_WIDTH>(self.col);
             self.cursor_position = add1::<BUFFER_WIDTH>(self.cursor_position);
         }
     }
